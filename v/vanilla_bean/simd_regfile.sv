17a9a3ebfe0a7333f7a72d6ff28ac261438b686e
max3145 <39097648+max3145@users.noreply.github.com>
2024-03-14 10:22:08 -0700

    Update simd_regfile.sv
    - changed the output to reflect previous regfile

--- v/vanilla_bean/simd_regfile.sv
+++ v/vanilla_bean/simd_regfile.sv
@@ -24,9 +24,7 @@ module simd_regfile
     
     , input [2:0] r_v_i
     , input [2:0][addr_width_lp-1:0] r_addr_i
-    , output logic [width_p-1:0] rs1_data_o //carries frs1
-    , output logic [width_p-1:0] rs2_data_o //carries frs2_scalar
-    , output logic [width_p-1:0] rs3_data_o //carries frs3
+    , output logic [2:0][width_p-1:0] r_data_o //carries frs1,2,3
     , output logic [2:0][width_p-1:0] rs2_simd_data_o //NEW OUTPUT, width 99
   );
 
@@ -42,9 +40,9 @@ module simd_regfile
 
   logic [3:0][width_p-1:0] mem_r [(els_p>>2)-1:0];
    
-  assign rs1_data_o = mem_r[r_addr_r[0][4:2]][r_addr_r[0][1:0]];
-  assign rs2_data_o = mem_r[r_addr_r[1][4:2]][r_addr_r[1][1:0]]; 
-  assign rs3_data_o = mem_r[r_addr_r[2][4:2]][r_addr_r[2][1:0]];
+  assign r_data_o[0] = mem_r[r_addr_r[0][4:2]][r_addr_r[0][1:0]];
+  assign r_data_o[1] = mem_r[r_addr_r[1][4:2]][r_addr_r[1][1:0]]; 
+  assign r_data_o[2] = mem_r[r_addr_r[2][4:2]][r_addr_r[2][1:0]];
   
   for (genvar i = 0; i < 3; i++)
      assign rs2_simd_data_o[i] = mem_r[r_addr_r[1][4:2]][i+1]; 
